02e7729ca4dc0c8c3bffe665c065cc0397e5a969
rafaelkoll <rafcall98@hotmail.com>
2022-04-13 01:53:29 +0100

    One hot encoding

--- src/VocabDataset.java
+++ src/VocabDataset.java
@@ -7,8 +7,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class VocabDataset  extends Dataset<double[], Integer> {
 
@@ -26,34 +26,67 @@ public class VocabDataset  extends Dataset<double[], Integer> {
         super(batchsize, shuffle, rnd);
     }
 
-    /**
-     * Load MNIST data from file.
-     */
     @Override
     public void fromFile(String path) throws IOException {
+    }
+
+
+    /**
+     * Load MNIST data from file and vocabulary.
+     */
+    public void fromFile(String path, String pathVocabulary) throws IOException {
         items = new ArrayList<Pair<double[], Integer>>();
 
-        //TODO: remove this
-        File myFile = new File(path);
-        System.out.println("Attempting to read from file in: "+myFile.getCanonicalPath());
+        System.out.println("here"+items.size());
 
-        BufferedReader br = new BufferedReader(new FileReader(path));
 
-        // first line
-        String[] ss = br.readLine().split(" ");
-        int size = Integer.valueOf(ss[0]);
-        inputDims = Integer.valueOf(ss[1]);
 
-        for (int i = 0; i < size; i++) {
-            ss = br.readLine().split(" ; ");
-            String[] sx = ss[0].split(" ");
-            double[] xs = new double[inputDims];
-            Integer y = Integer.valueOf(ss[1]);
-            for (int j = 0; j < sx.length; j++) {
-                xs[j] = Double.parseDouble(sx[j]);
-            }
-            items.add(new Pair<double[], Integer>(xs, y));
+        // the number of features and dimensions are equal.
+        //TODO: improve!
+        int instances = countLinesInFile(path);
+        int noFeatures = countLinesInFile(pathVocabulary);
+
+        System.out.println("instances: "+instances + " , features: "+ noFeatures);
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr);
+
+        for (int i = 0; i < 5; i++) {
+            String[] line = br.readLine().split(" ; ");
+            String locations = line[0]; // get all the locations (indexes) of the words.
+            int y = Integer.valueOf(line[1]);
+            System.out.println(locations + " y:"+y);
+            ArrayList<Integer> encoded = oneHotEncode(noFeatures, locations);
+
+            System.out.println(encoded.toString());
+
         }
+
         br.close();
+    }
+
+
+    public int countLinesInFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader( new FileReader(path));
+        int lines = 0;
+        while (br.readLine() != null) lines++;
+        br.close();
+        return lines;
+    }
+
+    public ArrayList<Integer> oneHotEncode(int noFeatures, String locations) {
+        // initialise with noFeatures of zeros.
+        ArrayList<Integer> encoded = new ArrayList<Integer>(Collections.nCopies(noFeatures, 0));
+        // Get all the indexes of the items in a list.
+        List<Integer> indexes = Arrays.stream(locations.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        // update zeros with 1 at all the indexes of elements.
+        for (int i=0; i<encoded.size(); i++) {
+            if (indexes.contains(i)) {
+                encoded.add(i, 1);
+            }
+        }
+        return encoded;
     }
 }
